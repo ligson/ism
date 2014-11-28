695d928d6592924d52b9fdd17d04fe6a1e7fcae7
zhengyunfei <zhengyf@ctyun.cn>
2014-11-28 15:36:13 +0800

    修改Admin C层

--- grails-app/controllers/com/ism/admin/controllers/AdminController.groovy
+++ grails-app/controllers/com/ism/admin/controllers/AdminController.groovy
@@ -1,10 +1,14 @@
 package com.ism.admin.controllers
 
+import com.ism.user.domains.User
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+
 class AdminController {
 
     def userService;
     def index() {
-        return redirect(action:'login')
+        return redirect(action:'index')
     }
     def login(){
         if(params.size()==2){
@@ -18,4 +22,23 @@ class AdminController {
             }
         }
     }
+  def list(){
+      JSONArray result;//返回的json
+      def page=params.page;
+      def rows=params.rows;
+      int intPage = Integer.parseInt((page == null || page == "0") ? "1":page);
+      //每页显示条数
+      int number = Integer.parseInt((rows == null || rows == "0") ? "10":rows);
+      //每页的开始记录  第一页为1  第二页为number +1
+      int start = (intPage-1)*number;
+      List<User> list=null;
+      int total=User.findAll().size();
+      if(total>0){
+          list=User.findAll();
+      }
+      Map<String, Object> jsonMap = new HashMap<String, Object>();//定义map
+      jsonMap.put("total", total);//total键 存放总记录数，必须的
+      jsonMap.put("rows", list);//rows键 存放每页记录 list
+      return render(view:'user',model:[result:jsonMap]);
+  }
 }
