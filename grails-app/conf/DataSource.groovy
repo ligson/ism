e82b4af2d3cc35f21cc12a489fef76eab78cd652
zhengyunfei <zhengyunfei>
2014-12-01 22:14:22 +0800

    这几个文件不提交不让我同步啊

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,7 +3,7 @@ dataSource {
     jmxExport = true
     driverClassName = "com.mysql.jdbc.Driver"
     username = "root"
-    password = "password"
+    password = "root"
 }
 hibernate {
     cache.use_second_level_cache = true
